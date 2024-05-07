57591c6a9b6226ed0c3b8447f467218e60fca239
JoaoAlexandre <alexandremjoao@gmail.com>
2024-05-07 16:34:40 -0300

    feat: "add Resultado Escolar, condicionais compostas"

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,12 +1,14 @@
 public class CaixaEletronico {
+   
+   //Condicionais Simples
     public static void main(String[] args) {
         double saldo = 250.0;
         double valorSolicitado = 170.0;
 
-        if (valorSolicitado < saldo) {
+        if (valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
-        }
         System.out.println(saldo);
+
 
     }
 }
